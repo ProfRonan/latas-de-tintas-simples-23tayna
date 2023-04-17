5b1e9a1113afd767c9a9eab17b89fce454347079
Adria Barreto <adriabarreto12@gmail.com>
2023-04-17 10:17:45 -0300

    pronto3

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ metros_quadrados = float(metros_quadrados)
 
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
-qtd_de_latas = 0
-valor_total = 0
+qtd_de_latas = 1
+valor_total = 80
 
-print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
+print(f"Serão necessárias {1} latas totalizando R$ {80}")
